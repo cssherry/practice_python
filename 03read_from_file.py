d16fda1fc710559d885926df5f260b25a17085ad
Sherry Zhou <sherry@arcadiadata.com>
2015-10-30 00:50:04 -0700

    Test if file exists in read from file script

--- 03read_from_file.py
+++ 03read_from_file.py
@@ -12,26 +12,30 @@ else:
 if filename[-4:] != ".txt":
     filename += ".txt"
 
-if exists(filename):
+file_exists = exists(filename)
+if file_exists:
     txt = open(filename)
     print "\nHere's your file %r:" % filename
     print txt.read()
     txt.close()
 else:
     print "\nThis will be a new file!"
-    print "\nIf you don't want that, hit CTRL-C (^C)."
+    print "If you don't want that, hit CTRL-C (^C)."
 
 # r+ writes like insert
 # a+ reads from end of file, writes to end of file, creates file if it doesn't exist
 # w+ will overwrite and read from end of file (file overwritten upon open)
 # r, w, and a will only do their respective tasks
-open_type = raw_input("Do you want to overwrite (w) or append (a)? ").lower()
-if open_type != "w" and open_type != "a" and open_type != "r":
-    print "Not a valid option. We're going to append"
-    open_type = "a"
+if file_exists:
+    open_type = raw_input("Do you want to overwrite (w) or append (a)? ").lower()
+    if open_type != "w" and open_type != "a" and open_type != "r":
+        print "Not a valid option. We're going to append"
+        open_type = "a"
+    print "\nNow we're going to do your action"
+    print "If you don't want that, hit CTRL-C (^C)."
+else:
+    open_type = "w"
 
-print "\nNow we're going to do your action"
-print "If you don't want that, hit CTRL-C (^C)."
 print "\nNow I'm going to ask you for three lines."
 
 txt = open(filename, open_type + "+")
