29ba494af89242b09866e2174d77983575f89e5b
Sherry Zhou <sherry@arcadiadata.com>
2015-10-30 00:50:34 -0700

    Shorter version of copy file script

---
+++ 04copy_file.py
@@ -0,0 +1,19 @@
+from sys import argv
+from os.path import exists
+
+def append_txt(filename):
+    if filename[-4:] != ".txt":
+        filename += ".txt"
+    return filename
+
+if len(argv) == 3:
+    script, from_file, to_file = argv
+else:
+    from_file = raw_input("What file do you want to copy? ")
+    to_file = raw_input("What do you want to call the copy? ")
+
+if exists(append_txt(from_file)):
+    open(append_txt(to_file), 'w').write(open(append_txt(from_file)).read())
+    print "Alright, all done."
+else:
+    print "FAILED: file doesn't exist."
