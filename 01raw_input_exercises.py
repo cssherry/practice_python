b42e3a126cd1fc52874d8f7235d427e86d6f1ebe
Sherry Zhou <sherry@arcadiadata.com>
2015-10-29 20:19:09 -0700

    Do some raw input exercises

---
+++ 01raw_input_exercises.py
@@ -0,0 +1,21 @@
+"""Functions used in http://learnpythonthehardway.org/book"""
+def get_input():
+    """This uses print statements to get input"""
+    print "How old are you?",
+    age = raw_input()
+    print "How tall are you?",
+    height = raw_input()
+    print "How much do you weigh?",
+    weight = raw_input()
+
+    print "So, you're %d old, %s tall and %d heavy." % (
+        int(age), str(height), int(weight))
+
+def get_input_shorter():
+    """This uses strings within print statements"""
+    age = raw_input("How old are you? ")
+    height = raw_input("How tall are you? ")
+    weight = raw_input("How much do you weigh? ")
+
+    print "So, you're %d old, %s tall and %d heavy." % (
+        int(age), str(height), int(weight))
